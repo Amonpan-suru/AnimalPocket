a88523fab7cf96239371ad0fad496cc8ddcca4bc
film110TH <61343092+film110TH@users.noreply.github.com>
2021-11-09 22:14:46 +0700

    UpDatePetStats2.0(CanSave)

--- AnimalPocet/Assets/Script/fish.cs
+++ AnimalPocet/Assets/Script/fish.cs
@@ -18,16 +18,25 @@ public class fish : PetStats
     public  float heallavel_C = 100;
 
 
-    public Text HU, HA, CL, HE;
+    public Text HU, HA, CL, HE,Name;
     
 
     private void Start()
-    {
-        hungrylavel = hungrylavel_C;
-        happinessLavel = happinessLavel_C;
-        cleanLavel = cleanLavel_C;
-        heallavel = heallavel_C;
-        SetStats();
+    {      
+
+        if(PlayerPrefs.HasKey(name + "hungryrate"))
+        {
+            Load();
+        }
+        else
+        {
+            RandStats();
+            hungrylavel = hungrylavel_C;
+            happinessLavel = happinessLavel_C;
+            cleanLavel = cleanLavel_C;
+            heallavel = heallavel_C;
+            SetStats();
+        }     
     }
 
     void SetStats()
@@ -36,9 +45,15 @@ public class fish : PetStats
         happinessrate = happinessrate_C;
         cleanlinessrate = cleanlinessrate_C;
         healrate = healrate_C;
-
     }
 
+    void RandStats()
+    {
+        hungryrate_C = Random.Range(4,10);
+        happinessrate_C = Random.Range(1,5);
+        cleanlinessrate_C = Random.Range(2, 10);
+        healrate_C = Random.Range(3, 6);    
+    }
 
     private void FixedUpdate()
     {
@@ -51,19 +66,7 @@ public class fish : PetStats
 
         UpdateStats();
         SetStats();
-
-
-        //if(lefttime >= 5)
-        //{
-        //    hungrylavel -= hungryrate;
-        //    happinessLavel -= happinessrate;
-        //    cleanLavel -= cleanlinessrate;
-        //    heallavel -= healrate;
-
-        //    UpdateStats();
-
-        //    lefttime = 0;
-        //}
+        Seve();
     }
 
     void UpdateStats()
@@ -77,7 +80,7 @@ public class fish : PetStats
         HA.text = "Happiness = " + happinessLavel;
         CL.text = "Clean = " + cleanLavel;
         HE.text = "Heal = " + heallavel;
-
+        Name.text = "Name = " + name;
     }
 
 
@@ -86,4 +89,30 @@ public class fish : PetStats
         happiness(happinessLavel);
     }
 
+    void Seve()
+    {
+        PlayerPrefs.SetFloat(name + "hungryrate", hungryrate_C);
+        PlayerPrefs.SetFloat(name + "happinessrate", happinessrate_C);
+        PlayerPrefs.SetFloat(name + "cleanlinessrate", cleanlinessrate_C);
+        PlayerPrefs.SetFloat(name + "healrate", healrate_C);
+
+        PlayerPrefs.SetFloat(name + "hungrylavel", hungrylavel);
+        PlayerPrefs.SetFloat(name + "happinessLavel", happinessLavel);
+        PlayerPrefs.SetFloat(name + "cleanLavel", cleanLavel);
+        PlayerPrefs.SetFloat(name + "heallavel", heallavel);
+
+        PlayerPrefs.Save();
+    }
+    void Load()
+    {
+        hungryrate_C = PlayerPrefs.GetFloat(name + "hungryrate");
+        happinessrate_C = PlayerPrefs.GetFloat(name + "happinessrate");
+        cleanlinessrate_C = PlayerPrefs.GetFloat(name + "cleanlinessrate");
+        healrate_C = PlayerPrefs.GetFloat(name + "healrate");
+
+        hungrylavel = PlayerPrefs.GetFloat(name + "hungrylavel");
+        happinessLavel = PlayerPrefs.GetFloat(name + "happinessLavel");
+        cleanLavel = PlayerPrefs.GetFloat(name + "cleanLavel");
+        heallavel = PlayerPrefs.GetFloat(name + "heallavel");
+    }
 }
